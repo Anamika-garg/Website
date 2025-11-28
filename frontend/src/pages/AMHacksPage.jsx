f35cd05350ab128c3d091140c3907ec6fdab7a12
Ishita Sati <ishasati19@gmail.com>
2025-11-28 12:24:23 +0530

    Add AMSponsi component to AMHacks page

--- frontend/src/pages/AMHacksPage.jsx
+++ frontend/src/pages/AMHacksPage.jsx
@@ -14,11 +14,13 @@ import Tagline from "../components/AMHacks/Tagline";
 import AMHacks from "../components/AMHacks/Hero";
 import Sponsor from "../components/AMHacks/sponsor";
 import FAQs from "../components/AMHacks/faqs";
+import AMSponsi from "../components/AMSponsi";
 
 const AMHacksPage = () => {
   return (
     <div className="top-[-90px] relative pt-24">
       <AMHacks/>
+      <AMSponsi/>
       <Sponsor/>
       <Tagline/>
       <FAQs/>
